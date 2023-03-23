c29ca256bd66baa48b0cfe433030a78981f5e1fa
Moitreya Chattopadhyay <121758854+moitreyacbx@users.noreply.github.com>
2023-03-24 03:52:48 +0530

    Delete Portfolio-Website directory

--- Portfolio-Website/script1.js
+++
@@ -1,6 +0,0 @@
-let name = "Moitreya";
-
-function disptext(){
-    const usernames = document.getElementById("MessageName").value;
-    document.getElementById("prompt").innerHTML = "Thank you for your message," + usernames;
-}
