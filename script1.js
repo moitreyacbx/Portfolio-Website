a94650e969c9f5cb67c4576281b5d443b3ee9362
Moitreya Chattopadhyay <121758854+moitreyacbx@users.noreply.github.com>
2023-03-24 03:50:49 +0530

    Add files via upload
    updated javascript file

---
+++ script1.js
@@ -0,0 +1,6 @@
+let name = "Moitreya";
+
+function disptext(){
+    const usernames = document.getElementById("MessageName").value;
+    document.getElementById("prompt").innerHTML = "Thank you for your message," + usernames;
+}
