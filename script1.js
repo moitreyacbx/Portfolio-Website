96c7203fb2dbc11bcc002ed605ed81e18612f85f
Moitreya Chattopadhyay <121758854+moitreyacbx@users.noreply.github.com>
2023-03-25 22:18:51 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,6 +1,15 @@
-let name = "Moitreya";
-
+let heading= document.body.children;
+heading[1].innerHTML = "Welcome to Moitreya's Portfolio Website";
 function disptext(){
     const usernames = document.getElementById("MessageName").value;
-    document.getElementById("prompt").innerHTML = "Thank you for your message," + usernames;
+    document.getElementById("prompt").innerHTML = "Thank you for your message," + usernames + "!";
+    document.querySelector("button#buttons.update").innerHTML = "Submitted";
+}
+function changemodedark(){
+    const el= document.querySelector("link");
+    el.href = "style2.css";
+}
+function changemodelight(){
+    const el= document.querySelector("link");
+    el.href = "style.css";
 }
