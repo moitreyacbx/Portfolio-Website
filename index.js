51156c612510e7fad30befc653a4cd402bcb658a
Moitreya Chattopadhyay <121758854+moitreyacbx@users.noreply.github.com>
2023-04-23 18:16:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(
   "mongodb+srv://moichat15:capitapiz@cluster0.1ax10zj.mongodb.net/Portfolio",
-  { useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000,
-    autoIndex: false, // Don't build indexes
-    maxPoolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6 
-  }
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 const schema = {
   message: "string",
-  name: "string"
+  name: "string",
 };
 
 const Message = mongoose.model("Message", schema);
@@ -34,16 +26,17 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   let newMessage = new Message({
     message: req.body.message,
-    name: req.body.name
+    name: req.body.name,
   });
-  newMessage.save()
-		.then(() => {
-			res.status(200).redirect('/');
-		})
-		.catch((error) => {
-			console.error(error);
-			res.status(500).send('Error saving message to database.');
-		});
+  newMessage
+    .save()
+    .then(() => {
+      res.status(200).redirect("/");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error saving message to database.");
+    });
 });
 
 app.listen(3000, () => {
