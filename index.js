77c624ed981c0e4d9fed3bd921cbfaab62383516
Moitreya Chattopadhyay <121758854+moitreyacbx@users.noreply.github.com>
2023-04-23 18:08:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(
   "mongodb+srv://moichat15:capitapiz@cluster0.1ax10zj.mongodb.net/Portfolio",
-  { useNewUrlParser: true, useUnifiedTopology: true }
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6 
+  }
 );
 
 const schema = {
